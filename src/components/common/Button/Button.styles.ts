61a9bc3a5dda927f0c3055df86fd70566a4d893c
André Daniel <andre.fervilha@gmail.com>
2021-01-13 21:46:57 -0300

    ✨ Add Button component

---
+++ src/components/common/Button/Button.styles.ts
@@ -0,0 +1,50 @@
+import styled, { css } from 'styled-components'
+import { theme } from '../../../theme'
+import { IButton } from './Button'
+
+export const ButtonWrapper = styled.div<IButton>`
+  padding: 0.5rem 0;
+  width: 100%;
+
+  a,
+  button {
+    background: ${theme.colors.secondary.LIGHT};
+    border-radius: 5px;
+    border: 1px solid ${theme.colors.secondary.MAIN};
+    font-size: 1rem;
+    padding: 0.7rem 1rem;
+    position: relative;
+    transition: background-color 200ms ease, border-color 200ms ease,
+      color 200ms ease;
+    width: 100%;
+
+    &:focus {
+      outline: none;
+    }
+
+    &:hover {
+      background-color: ${theme.colors.secondary.CONTRAST};
+    }
+  }
+
+  ${({ variant }) => {
+    switch (variant) {
+      case 'primary':
+        return css`
+          a,
+          button {
+            border: none;
+            background: ${theme.colors.success.LIGHT};
+            color: #fff;
+
+            &:hover {
+              background-color: ${theme.colors.success.MAIN};
+            }
+          }
+        `
+
+      default:
+        break
+    }
+  }}
+`
---
+++ src/components/common/Button/Button.tsx
@@ -0,0 +1,29 @@
+import React, { ButtonHTMLAttributes } from 'react'
+import Ink from 'react-ink'
+import { Link } from 'react-router-dom'
+import { ButtonWrapper } from './Button.styles'
+
+export type VariationTypes = 'primary' | 'secondary'
+
+export interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {
+  label?: string
+  variant?: VariationTypes
+  href?: string
+}
+
+const Button: React.FC<IButton> = ({ children, ...props }): JSX.Element => {
+  const { href, label, id, type, variant, ...rest } = props
+
+  const component = (
+    <ButtonWrapper variant={variant}>
+      <button type={type || 'button'} id={id} {...rest}>
+        {children || label}
+        <Ink opacity={0.1} />
+      </button>
+    </ButtonWrapper>
+  )
+
+  return <>{href ? <Link to={href}>{component}</Link> : component}</>
+}
+
+export { Button }
