fb24c9d8fd73763e1443216784d2143aed109c2a
André Daniel <andre.fervilha@gmail.com>
2021-01-13 21:49:17 -0300

    ✨ Add AppBar component

---
+++ src/components/core/AppBar/AppBar.styles.ts
@@ -0,0 +1,127 @@
+import styled from 'styled-components'
+import { theme } from '../../../theme'
+
+export const AppBarWrapper = styled.div`
+  bottom: 0;
+  bottom: 0;
+  display: flex;
+  flex-direction: row;
+  left: 0;
+  padding: 0 5px;
+  position: fixed;
+  width: 100vw;
+
+  @media (min-width: 768px) {
+    flex-direction: column;
+    max-width: 300px;
+    padding: 0 30px;
+    top: 80px;
+  }
+`
+
+export const List = styled.div`
+  display: grid;
+  flex-direction: row;
+  grid-template-columns: repeat(4, 25%);
+  justify-content: space-between;
+  width: 100%;
+
+  @media (min-width: 768px) {
+    flex-direction: column;
+    grid-template-columns: 1fr;
+  }
+`
+
+export const ListItem = styled.div`
+  margin: 5px 0;
+  position: relative;
+
+  svg {
+    height: 1.5rem;
+    margin-bottom: 5px;
+    transition: fill 200ms ease;
+    width: 1.5rem;
+
+    @media (min-width: 768px) {
+      margin-bottom: 0;
+      margin-right: 1rem;
+    }
+  }
+
+  a {
+    align-items: center;
+    color: inherit;
+    display: flex;
+    flex-direction: column;
+    font-size: 0.75rem;
+    padding: 15px 10px;
+    position: relative;
+    text-decoration: none;
+    transition: color 200ms ease;
+    z-index: 1;
+
+    @media (min-width: 768px) {
+      flex-direction: row;
+      font-size: 1rem;
+      padding: 10px 20px;
+    }
+
+    > span {
+      display: block;
+      max-width: 100%;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+
+    &:after,
+    &:before {
+      border-radius: 5px;
+      content: '';
+      position: absolute;
+      transition: all 200ms ease-in-out;
+    }
+
+    &:before {
+      height: 100%;
+      left: 0;
+      top: 0;
+      transform: scale(0.95);
+      width: 100%;
+      z-index: -1;
+    }
+
+    &:after {
+      filter: blur(5px);
+      height: 90%;
+      left: 5%;
+      opacity: 0;
+      top: 20%;
+      width: 90%;
+      z-index: -2;
+    }
+
+    &.active,
+    &:hover,
+    &:focus,
+    &:active {
+      color: ${theme.colors.secondary.CONTRAST};
+      outline: none;
+
+      &:before {
+        background-color: ${theme.colors.success.LIGHT};
+        transform: scale(1);
+      }
+
+      &:after {
+        background-color: ${theme.colors.success.LIGHT};
+        opacity: 0.5;
+      }
+    }
+
+    &:focus,
+    &:active {
+      opacity: 0.7;
+    }
+  }
+`
---
+++ src/components/core/AppBar/AppBar.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { FiHome, FiLogOut, FiPlusSquare, FiShoppingBag } from 'react-icons/fi'
+import Ink from 'react-ink'
+import { NavLink } from 'react-router-dom'
+import {
+  AppBarWrapper,
+  List,
+  ListItem as StyledListItem,
+} from './AppBar.styles'
+
+interface IProps {
+  to: string
+  icon: JSX.Element
+  label: string
+  exact?: boolean
+}
+
+const ListItem: React.FC<IProps> = (props): JSX.Element => {
+  const { to, icon, label, exact = false } = props
+
+  return (
+    <StyledListItem>
+      <NavLink to={to} exact={exact}>
+        {icon}
+        <span>{label}</span>
+        <Ink opacity={0.1} />
+      </NavLink>
+    </StyledListItem>
+  )
+}
+
+const AppBar: React.FC = ({ children }): JSX.Element => {
+  return (
+    <AppBarWrapper>
+      <List>
+        <ListItem to="/" icon={<FiHome />} label="Dashboard" exact />
+        <ListItem
+          to="/orders/create"
+          icon={<FiPlusSquare />}
+          label="Cadastrar compra"
+          exact
+        />
+        <ListItem
+          to="/orders"
+          icon={<FiShoppingBag />}
+          label="Minhas compras"
+          exact
+        />
+        <ListItem to="/logout" icon={<FiLogOut />} label="Sair" />
+      </List>
+    </AppBarWrapper>
+  )
+}
+
+export { AppBar }
