05ece72f8258ac50fcb40ede2aa4fd9b4051d3bb
André Daniel <andre.fervilha@gmail.com>
2021-01-13 21:21:40 -0300

    ✨ Add Auth layout component

---
+++ src/components/layouts/Auth/Auth.styles.ts
@@ -0,0 +1,79 @@
+import styled from 'styled-components'
+import { theme } from '../../../theme'
+
+export const AuthWrapper = styled.div`
+  background-color: ${theme.colors.primary.DARK};
+  background-image: linear-gradient(
+    132deg,
+    ${theme.colors.primary.MAIN} 0%,
+    ${theme.colors.primary.DARK} 100%
+  );
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  min-height: 100vh;
+  padding: 50px;
+  position: relative;
+
+  &:after {
+    background-image: url('/images/login-background-pattern.png');
+    content: '';
+    height: 100%;
+    left: 0;
+    pointer-events: none;
+    position: absolute;
+    top: 0;
+    width: 100%;
+    z-index: 1;
+  }
+`
+
+export const AuthContent = styled.div`
+  align-items: center;
+  background-repeat: repeat;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 2rem;
+
+  .head {
+    margin: 2rem 0;
+    text-align: center;
+  }
+
+  svg {
+    height: auto;
+    max-width: 200px;
+  }
+
+  h1 {
+    color: ${theme.colors.primary.LIGHT};
+    font-size: 25px;
+    font-weight: normal;
+    margin: 0;
+  }
+
+  p {
+    color: #666;
+    margin: 5px 0 0;
+  }
+`
+
+export const AuthCover = styled.div`
+  background-image: url('/images/login-picture.png');
+  background-position: center;
+  background-size: cover;
+  height: 100%;
+  width: 100%;
+`
+
+export const Card = styled.div`
+  background-color: #fff;
+  border-radius: 10px;
+  display: grid;
+  grid-template-columns: 2fr 3fr;
+  margin: 0 auto;
+  max-width: 1100px;
+  overflow: hidden;
+  width: 100%;
+`
---
+++ src/components/layouts/Auth/Auth.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { ReactComponent as Brand } from '../../../assets/brand/logo-black.svg'
+import { AuthContent, AuthCover, AuthWrapper, Card } from './Auth.styles'
+
+const Auth: React.FC = ({ children }): JSX.Element => {
+  return (
+    <AuthWrapper>
+      <Card>
+        <AuthContent>
+          <Brand />
+          {children}
+        </AuthContent>
+        <AuthCover />
+      </Card>
+    </AuthWrapper>
+  )
+}
+
+export { Auth }
