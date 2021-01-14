66db09c314a87da273fc4bfc910538bec932677c
André Daniel <andre.fervilha@gmail.com>
2021-01-13 21:51:30 -0300

    ✨ Add NavBar component

---
+++ src/components/core/NavBar/NavBar.styles.ts
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+import { theme } from '../../../theme'
+
+export const NavBarWrapper = styled.div`
+  align-items: center;
+  display: flex;
+  height: 80px;
+  justify-content: space-between;
+  left: 0;
+  padding: 0 20px;
+  position: fixed;
+  top: 0;
+  width: 100%;
+
+  @media (min-width: 768px) {
+    padding: 0 50px;
+  }
+
+  .brand-wrapper {
+    svg {
+      height: auto;
+      margin-right: 10px;
+      width: 150px;
+    }
+  }
+
+  .user-info {
+    align-items: center;
+    color: ${theme.colors.secondary.DARK};
+    display: flex;
+
+    img {
+      margin-right: 10px;
+    }
+  }
+`
---
+++ src/components/core/NavBar/NavBar.tsx
@@ -0,0 +1,26 @@
+import * as CryptoJS from 'crypto-js'
+import React from 'react'
+import { ReactComponent as Brand } from '../../../assets/brand/logo-black.svg'
+import { Avatar } from '../../common/Avatar'
+import { NavBarWrapper } from './NavBar.styles'
+
+const NavBar: React.FC = (): JSX.Element => {
+  const avatarUrl = `https://www.gravatar.com/avatar/${CryptoJS.MD5(
+    'andre.fervilha@gmail.com'
+  )}`
+
+  return (
+    <NavBarWrapper>
+      <div className="brand-wrapper">
+        <Brand />
+        <span>Cashback</span>
+      </div>
+      <div className="user-info">
+        <Avatar src={avatarUrl} />
+        <span className="welcome-message">Bem-vindo, André!</span>
+      </div>
+    </NavBarWrapper>
+  )
+}
+
+export { NavBar }
