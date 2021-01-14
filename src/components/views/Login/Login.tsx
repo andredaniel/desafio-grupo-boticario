feff4702739eb32c27c34f7b708cdb07c5be3883
André Daniel <andre.fervilha@gmail.com>
2021-01-13 21:26:14 -0300

    ✨ Add Login page

---
+++ src/components/views/Login/Login.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Button, Input } from '../../common'
+import { Auth } from '../../layouts/Auth'
+
+const Login: React.FC = (): JSX.Element => {
+  return (
+    <Auth>
+      <div className="head">
+        <h1>Login</h1>
+      </div>
+      <Input
+        id="email"
+        type="email"
+        label="E-mail"
+        placeholder="Digite seu e-mail"
+      />
+      <Input
+        id="password"
+        type="password"
+        label="Senha"
+        placeholder="Digite sua senha"
+      />
+      <Button variant="primary" label="Acessar" />
+      <hr />
+      <Button
+        href="/register"
+        variant="secondary"
+        label="Quero ser um revendedor"
+      />
+    </Auth>
+  )
+}
+
+export { Login }
