37a1d9f16ed29c53097c5541b6f5eae9c1fd05af
André Daniel <andre.fervilha@gmail.com>
2021-01-13 22:39:58 -0300

    💄 Fix layout on mobile devices

--- src/components/layouts/Auth/Auth.styles.ts
+++ src/components/layouts/Auth/Auth.styles.ts
@@ -12,8 +12,12 @@ export const AuthWrapper = styled.div`
   display: flex;
   justify-content: center;
   min-height: 100vh;
-  padding: 50px;
+  padding: 50px 20px;
   position: relative;
+  
+  @media (min-width: 768px) {
+    padding: 50px;
+  }
 
   &:after {
     background-image: url('/images/login-background-pattern.png');
@@ -45,6 +49,7 @@ export const AuthContent = styled.div`
   padding: 2rem;
   position: relative;
   z-index: 2;
+  order: 2;
 
   .head {
     margin: 2rem 0;
@@ -71,7 +76,7 @@ export const AuthContent = styled.div`
 
 export const AuthCover = styled.div`
   background-image: url('/images/login-picture.png');
-  background-position: center;
+  background-position: 60% center;
   background-size: cover;
   height: 100%;
   width: 100%;
@@ -81,9 +86,16 @@ export const Card = styled.div`
   background-color: #fff;
   border-radius: 10px;
   display: grid;
-  grid-template-columns: 2fr 3fr;
+  grid-template-columns: 1fr;
+  grid-template-rows: 200px 3fr;
   margin: 0 auto;
   max-width: 1100px;
   overflow: hidden;
   width: 100%;
+  order: 1;
+  
+  @media (min-width: 768px) {
+    grid-template-columns: 2fr 3fr;
+    grid-template-rows: 1fr;
+  }
 `
