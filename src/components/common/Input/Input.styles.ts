22128d37b4b21e5bddcb3e80233836e881472d23
André Daniel <andre.fervilha@gmail.com>
2021-01-13 21:43:42 -0300

    ✨ Add Input component

---
+++ src/components/common/Input/Input.styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const InputWrapper = styled.div`
+  padding: 0.5rem 0;
+  width: 100%;
+
+  label {
+    display: block;
+    margin-bottom: 5px;
+  }
+  
+  input {
+    background: #fff;
+    border-radius: 0.4rem;
+    border: 1px solid #ddd;
+    padding: 0.7rem 1rem;
+    width: 100%;
+    font-size: 1rem;
+  }
+`
---
+++ src/components/common/Input/Input.tsx
@@ -0,0 +1,19 @@
+import React, { InputHTMLAttributes } from 'react'
+import { InputWrapper } from './Input.styles'
+
+interface IInput extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string
+}
+
+const Input: React.FC<IInput> = (props): JSX.Element => {
+  const { label, id, type, ...rest } = props
+
+  return (
+    <InputWrapper>
+      {label && <label htmlFor={id}>{label}</label>}
+      <input type={type || 'text'} id={id} {...rest} />
+    </InputWrapper>
+  )
+}
+
+export { Input }
