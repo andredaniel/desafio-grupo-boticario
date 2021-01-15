d52e419cee8e2b2d0069b624139c75512b8270f9
André Daniel <andre.fervilha@gmail.com>
2021-01-15 16:59:51 -0300

    🚚 Update folders structure

--- src/pages/Home/Home.styles.ts
+++ src/pages/Home/Home.styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { theme } from '../../../theme'
+import { theme } from '../../theme'
 
 export const Flex = styled.div`
   display: flex;
--- src/pages/Logout/Logout.tsx
+++ src/pages/Logout/Logout.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Redirect } from 'react-router-dom'
-import { logout } from '../../../hooks/auth'
+import { logout } from '../../hooks/auth'
 
 const Logout: React.FC = (): JSX.Element => {
   logout()
