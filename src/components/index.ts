8b2deee9563f9f827b2259931ec96ab7f5ed2894
André Daniel <andre.fervilha@gmail.com>
2021-01-17 13:55:11 -0300

    🎨 Export new components from index

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,8 @@
 import Avatar from './Avatar'
 import Button from './Button'
+import Card from './Card'
 import Input from './Input'
+import Status from './Status'
+import Table from './Table'
 
-export { Avatar, Button, Input }
+export { Avatar, Button, Card, Input, Status, Table }
