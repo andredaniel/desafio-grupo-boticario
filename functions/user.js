2a9d5cc9f4445b80fa61cb2f6a3e1a9f28347253
André Daniel <andre.fervilha@gmail.com>
2021-01-18 04:13:28 -0300

    ✨ Add Netlify lambda function to fetch user data

---
+++ functions/user.js
@@ -0,0 +1,41 @@
+const axios = require('axios')
+
+const randomStatus = () => {
+  const key = Math.floor(Math.random() * 3)
+
+  const statuses = ['APPROVED', 'DISAPPROVED', 'IN_VALIDATION']
+
+  return statuses[key]
+}
+
+exports.handler = async function (event, context) {
+  console.log(event)
+  const baseUrl = 'https://5fffb468cb21e10017af7e99.mockapi.io/api'
+
+  if (event.httpMethod === 'GET') {
+    const { data } = await axios.get(
+      `${baseUrl}/orders/?user_id=${event.queryStringParameters.user_id}`
+    )
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    }
+  }
+
+  if (event.httpMethod === 'POST') {
+    const newOrder = JSON.parse(event.body)
+
+    newOrder.cashbackAppliedPercentage = Math.ceil(Math.random() * 20)
+    newOrder.cashbackReceivedValue =
+      (newOrder.value / 100) * newOrder.cashbackAppliedPercentage
+    newOrder.status = randomStatus()
+
+    const { data } = await axios.post(`${baseUrl}/orders`, newOrder)
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    }
+  }
+}
