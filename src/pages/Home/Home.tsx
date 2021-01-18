9934198553559845eb8bb6a8f53d532f328d7c2b
André Daniel <andre.fervilha@gmail.com>
2021-01-18 04:22:20 -0300

    ✨ Display user date

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,24 +1,41 @@
 import React from 'react'
 import { ReactComponent as Illustration } from '../../assets/images/jewelry.svg'
+import { useUser } from '../../contexts/user.context'
 import { Panel } from '../../layouts/Panel'
-import { StyledCard, Flex, HomeWrapper, IllustrationWrapper } from './Home.styles'
+import { IOrder } from '../../types/user.interface'
+import { formatMoney } from '../../utils/money.utils'
+import {
+  Flex,
+  HomeWrapper,
+  IllustrationWrapper,
+  StyledCard,
+} from './Home.styles'
 
 const Home: React.FC = (): JSX.Element => {
+  const { orders } = useUser()
+
   return (
     <Panel overflow="visible">
       <HomeWrapper>
         <h1 className="title">Meu cashback</h1>
         <Flex>
           <StyledCard>
-            <h2>R$ 78,40</h2>
+            <h2>
+              {formatMoney(
+                orders.reduce((acc: number, order: IOrder) => {
+                  acc = acc + Number(order.cashbackReceivedValue)
+                  return acc
+                }, 0)
+              )}
+            </h2>
             <p>Valor em cashback</p>
           </StyledCard>
           <StyledCard>
-            <h2>14</h2>
+            <h2>{orders.length}</h2>
             <p>Compras realizadas</p>
           </StyledCard>
           <StyledCard>
-            <h2>65</h2>
+            <h2>{Math.ceil(Math.random() * orders.length)}</h2>
             <p>Vendas realizadas</p>
           </StyledCard>
         </Flex>
--- src/pages/Orders/Orders.tsx
+++ src/pages/Orders/Orders.tsx
@@ -1,34 +1,26 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { FiMoreVertical } from 'react-icons/fi'
+import { FcShop } from 'react-icons/fc'
 import { Status, Table } from '../../components'
+import { useUser } from '../../contexts/user.context'
 import { Panel } from '../../layouts/Panel'
 import { StatusTypes } from '../../types/status.interface'
-import { IUser } from '../../types/user.interface'
+import { IOrder } from '../../types/user.interface'
 import { formatMoney } from '../../utils/money.utils'
 
 const Orders: React.FC = (): JSX.Element => {
-  const [user, setUser] = useState<IUser | null>(null)
+  const { user, orders, setOrders } = useUser()
   const [isLoading, setIsLoading] = useState<boolean>(true)
 
   useEffect(() => {
-    axios.get('/.netlify/functions/user').then(({ data }) => {
-      setUser(data)
-      setIsLoading(false)
-    })
-  }, [])
-
-  const randomStatus = () => {
-    const key = Math.floor(Math.random() * 3)
-
-    const statuses = [
-      StatusTypes.APPROVED,
-      StatusTypes.DISAPPROVED,
-      StatusTypes.IN_VALIDATION,
-    ]
-
-    return statuses[key]
-  }
+    axios
+      .get(`/.netlify/functions/user/?user_id=${user.id}`)
+      .then(({ data }) => {
+        setOrders(data)
+        setIsLoading(false)
+      })
+  }, [setOrders, user.id])
 
   return (
     <Panel>
@@ -46,27 +38,38 @@ const Orders: React.FC = (): JSX.Element => {
           </tr>
         </thead>
         <tbody>
-          {user?.orders.map((order) => {
-            return (
-              <tr key={order.id}>
-                <td>{order.code}</td>
-                <td>
-                  <strong>{formatMoney(order.value)}</strong>
-                </td>
-                <td>{new Date(order.date * 1000).toLocaleDateString()}</td>
-                <td>{order.cashbackAppliedPercentage}%</td>
-                <td>
-                  <strong>{formatMoney(order.cashbackReceivedValue)}</strong>
-                </td>
-                <td>
-                  <Status type={randomStatus()} />
-                </td>
-                <td align="right">
-                  <FiMoreVertical />
-                </td>
-              </tr>
-            )
-          })}
+          {orders.length > 0 ? (
+            orders.map((order: IOrder) => {
+              return (
+                <tr key={order.id}>
+                  <td>{order.code}</td>
+                  <td>
+                    <strong>{formatMoney(order.value)}</strong>
+                  </td>
+                  <td>{order.date}</td>
+                  <td>{order.cashbackAppliedPercentage}%</td>
+                  <td>
+                    <strong>{formatMoney(order.cashbackReceivedValue)}</strong>
+                  </td>
+                  <td>
+                    <Status type={order.status} />
+                  </td>
+                  <td align="right">
+                    <FiMoreVertical />
+                  </td>
+                </tr>
+              )
+            })
+          ) : (
+            <tr style={{ textAlign: 'center' }}>
+              <td colSpan={7}>
+                <div style={{ fontSize: 100, lineHeight: 0 }}>
+                  <FcShop />
+                </div>
+                <span>Sem compras registradas, ainda.</span>
+              </td>
+            </tr>
+          )}
         </tbody>
       </Table>
     </Panel>
